74559a61b43f346271352507b0de6a35d2596759
p3zz <federico.pezzato.work@gmail.com>
2025-06-10 22:13:14 +0200

    add receive part on client side

--- src/client.c
+++ src/client.c
@@ -4,15 +4,17 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP   "127.0.0.1"
+#define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 12345
-#define MESSAGE     "Hello from UDP client"
+#define BUFFER_SIZE 1024
 
 int main() {
     int sockfd;
     struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+    const char *message = "Hello from client";
 
-    // Create UDP socket
+    // Create socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
@@ -22,23 +24,39 @@ int main() {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
-
     if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
-        perror("Invalid server IP address");
+        perror("Invalid address");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
 
-    // Send message
-    if (sendto(sockfd, MESSAGE, strlen(MESSAGE), 0,
-               (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("sendto failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
+    while(1){
+        // Send message to server
+        if (sendto(sockfd, message, strlen(message), 0,
+                (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+            perror("sendto failed");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Wait for response
+        socklen_t addr_len = sizeof(server_addr);
+        ssize_t n = recvfrom(sockfd, buffer, BUFFER_SIZE - 1, 0,
+                            (struct sockaddr *)&server_addr, &addr_len);
+        if (n < 0) {
+            perror("recvfrom failed");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Received from server %ld bytes\n", n);
+
+        for(int i=0;i<n;i++){
+            printf("%.2X", buffer[i]);
+        }
+        printf("\n");
+        sleep(1); // simulate work / wait time
     }
-
-    printf("Message sent to %s:%d\n", SERVER_IP, SERVER_PORT);
-
     close(sockfd);
     return 0;
 }
